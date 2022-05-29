415f539b2f7f39d6f43d3c396748827115cfd142
Oyewande Oluwaseun <seunemmanuel2255@gmail.com>
2022-05-29 14:19:58 +0100

    feature: main app / home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import "./App.css";
+import { usePapaParse } from "react-papaparse";
+import CreateQueryView from "./components/createQueryView";
+import PredefinedQueryView from "./components/predefinedQueryView";
+import ViewDatabase from "./components/viewDatabase";
+import urls from "./urls";
 
 function App() {
+  const { readRemoteFile } = usePapaParse();
+  const [view, setView] = useState("home");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    var tables = [];
+    // eslint-disable-next-line array-callback-return
+    urls.map((url, id) => {
+      readRemoteFile(url, {
+        complete: (results) => {
+          tables[id] = results;
+        },
+      });
+    });
+    setData(tables);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
+    // Home Page / Main View
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Button
+        variant="contained"
+        style={muiStyles.databaseButton}
+        onClick={() => {
+          setView("viewDatabase");
+        }}
+      >
+        Database
+      </Button>
+      <header>
+        <h1 className="header">
+          Practice running SQL queries with our mock database
+        </h1>
       </header>
+      <section className="container">
+        <div className="button__container">
+          <Button
+            variant="contained"
+            style={muiStyles.createQueryButton}
+            onClick={() => {
+              setView("createQuery");
+            }}
+          >
+            <span className="app__button__text">Create Query</span>
+          </Button>
+          <Button
+            variant="contained"
+            style={muiStyles.predefinedQueryButton}
+            onClick={() => {
+              setView("predefinedQueryView");
+            }}
+          >
+            <span className="app__button__text">Run Predefined queries</span>
+          </Button>
+        </div>
+      </section>
+
+      {/* Views */}
+      {view === "createQuery" && (
+        <CreateQueryView setView={setView} data={data} />
+      )}
+      {view === "predefinedQueryView" && (
+        <PredefinedQueryView setView={setView} data={data} />
+      )}
+      {view === "viewDatabase" && (
+        <ViewDatabase setView={setView} data={data} />
+      )}
     </div>
   );
 }
+
+//style for MUI components
+const muiStyles = {
+  databaseButton: {
+    backgroundColor: "rgb(17, 4, 90)",
+    position: "absolute",
+    fontWeight: "500",
+    top: "10px",
+    right: "10px",
+    color: "white",
+    padding: "5px",
+  },
+  predefinedQueryButton: {
+    backgroundColor: "rgb(17, 4, 90)",
+    padding: "5px",
+  },
+  createQueryButton: {
+    backgroundColor: "rgb(17, 4, 90)",
+    padding: "5px",
+    marginBottom: "40px",
+  },
+};
 
 export default App;
