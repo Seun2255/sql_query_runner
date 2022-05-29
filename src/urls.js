5d672b0695410248edf76d6dd6f438c62e09dbe8
Oyewande Oluwaseun <seunemmanuel2255@gmail.com>
2022-05-29 14:20:43 +0100

    feature: database urls list

---
+++ src/urls.js
@@ -0,0 +1,12 @@
+const customers =
+  "https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/csv/customers.csv";
+const suppliers =
+  "https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/csv/suppliers.csv";
+const territories =
+  "https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/csv/territories.csv";
+const products =
+  "https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/csv/products.csv";
+
+const urls = [customers, suppliers, products, territories];
+
+export default urls;
